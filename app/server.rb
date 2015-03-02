ad792d984714060145202ac873dde83977041a2c
tommasobratto <tbratto22@gmail.com>
2015-03-02 13:05:39 +0000

    added sign up feature

--- app/server.rb
+++ app/server.rb
@@ -6,10 +6,15 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
 require_relative './models/message'
+require_relative './models/user'
+require_relative 'helpers'
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
+enable :sessions
+set :session_secret, 'secret'
 
 get '/' do 
   @messages = Message.all
@@ -20,3 +25,14 @@ post '/messages' do
   Message.create(content: params[:content])
   redirect to '/'
 end
+
+get '/users/new' do 
+  erb :"/users/new"
+end
+
+post '/users' do 
+  user = User.create(name: params[:name],email: params[:email], password: params[:password])
+  session[:user_id] = user.id
+  redirect to '/'
+end
+
---
+++ spec/user_signup_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature "User signs up" do 
+
+  scenario "as a new user" do 
+    expect{ sign_up }.to change(User, :count).by 1
+    expect(page).to have_content "Welcome John117"
+    expect(User.first.name).to eq "John117"
+  end
+
+  def sign_up(name = "John117", email = "mcpo@unsc.com", password = "0451")
+    visit '/users/new'
+    expect(page.status_code).to eq 200
+    fill_in :name, with: "John117"
+    fill_in :email, with: "mcpo@unsc.com"
+    fill_in :password, with: "0451"
+    click_on 'sign up'
+  end
+end
